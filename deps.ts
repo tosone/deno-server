15bbedbc4c2dcada2e80da315abc1d8c79dda006
Tosone <i@tosone.cn>
2021-02-28 01:16:14 +0800

    :heavy_plus_sign: Add mongo deps.

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
 export type { RouterContext } from "https://deno.land/x/oak/mod.ts";
+export { Bson, MongoClient } from "https://deno.land/x/mongo/mod.ts";
